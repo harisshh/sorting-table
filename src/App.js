fa544e581bc2a9fec0202180d604e3e786f4d578
Harish Sridhar <harishsridhar@Harishs-MacBook-Air.local>
2023-04-05 17:10:52 +0530

    sortable react table

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { SortingTable } from './components/sortingTable';
+
+function App() {
+  return (
+    <div className='App'>
+      <SortingTable/>
+    </div>
+  )
+}
+
+export default App
